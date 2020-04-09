45677abca3910552dddb5cc072d17e5c079b23a7
Eivile Augyte <eivileaugyte@gmail.com>
2020-04-09 17:55:53 +0300

    Added my version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment: Caching the Inverse of a Matrix
+## This code helps to cache the inverse of a matrix by executing few functions
 
-## Write a short comment describing this function
+## This function creates a special matrix object that can cache its inverse
+makeCacheMatrix <- function( x = matrix() ) {
+  
+  ## Initialize the inverse property
+  i <- NULL
+  
+  ## set the value of the matrix
+  set <- function( matrix ) {
+    x <<- matrix
+    i <<- NULL
+  }
+  
+  ## get the value of the matrix
+  get <- function() {
+    ## return the value of the matrix
+    x
+  }
 
-makeCacheMatrix <- function(x = matrix()) {
-
+  ## set the value of the inverse
+  setInverse <- function(inverse) {
+    i <<- inverse
+  }
+  
+  ## get the value of the inverse
+  getInverse <- function() {
+    ## return the value of the inverse
+    i
+  }
+  
+  ## return a list of matrices
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special “matrix” returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not
+## changed), then the "cacheSolve" should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  
+  ## return the inverse (if its already set)
+  if( !is.null(m) ) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  ## get the matrix from our object
+  data <- x$get()
+  
+  ## calculate the inverse using matrix multiplication
+  m <- solve(data) %*% data
+  
+  ## set the inverse to the object
+  x$setInverse(m)
+  
+  ## return the matrix
+  m
 }
